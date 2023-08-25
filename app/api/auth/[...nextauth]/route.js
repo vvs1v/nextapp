2d1a8f0b9a3053ab34674bec1d722c8db5da8c62
unknown <vaibhavsinghal2000@gmail.com>
2023-08-25 09:34:00 +0530

    second commit

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -17,8 +17,8 @@ const handler = NextAuth({
         })
     ],
 
-    callback:{
-    async session({ session }){
+    callback :{
+    async session({ session }) {
         const sessionUser = await userAgent.findOne({
             email:session.user.email
         })
@@ -34,7 +34,7 @@ const handler = NextAuth({
          console.log("mongoose connected")
          //check if a user already exist
          const userExists = await User.findOne({
-            email:profile.email
+            email: profile.email
          });
 
 
@@ -57,4 +57,45 @@ const handler = NextAuth({
         }
     }},
 })
+
+
+// const handler = NextAuth({
+//     providers: [
+//       GoogleProvider({
+//         clientId: process.env.GOOGLE_ID,
+//         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//       })
+//     ],
+//     callbacks: {
+//       async session({ session }) {
+//         // store the user id from MongoDB to session
+//         const sessionUser = await User.findOne({ email: session.user.email });
+//         session.user.id = sessionUser._id.toString();
+  
+//         return session;
+//       },
+//       async signIn({ account, profile, user, credentials }) {
+//         try {
+//           await connectToDB();
+  
+//           // check if user already exists
+//           const userExists = await User.findOne({ email: profile.email });
+  
+//           // if not, create a new document and save user in MongoDB
+//           if (!userExists) {
+//             await User.create({
+//               email: profile.email,
+//               username: profile.name.replace(" ", "").toLowerCase(),
+//               image: profile.picture,
+//             });
+//           }
+  
+//           return true
+//         } catch (error) {
+//           console.log("Error checking if user exists: ", error.message);
+//           return false
+//         }
+//       },
+//     }
+//   })
 export {handler as GET, handler as POST};
